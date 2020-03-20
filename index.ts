8c4f13320adae68d07906c0c4458cd3e8f066527
Jose Mendoza <43755829+hndrjose@users.noreply.github.com>
2020-03-20 15:39:57 -0600

    onciavo upload del backend

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ server.app.use( bodyParser.urlencoded({ extended: true }) );
 server.app.use( bodyParser.json() );
 
 // CORS
-server.app.use( cors({ origin: '*', credentials: true  }) );
+server.app.use( cors());
 
 
 // Rutas de servicios
